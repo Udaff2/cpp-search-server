b958cbaf65e071c44c6b590ed7adcc5dca01c46d
Udaff2 <pobocon@mail.ru>
2021-12-29 10:30:47 +0500

    add my solution from the lesson Divide the project into files

---
+++ search-server/string_processing.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> SplitIntoWords(const string& text);
+
+template <typename StringContainer>
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+  set<string> non_empty_strings;
+  for (const string& str : strings) {
+    if (!str.empty()) {
+      non_empty_strings.insert(str);
+    }
+  }
+  return non_empty_strings;
+}
